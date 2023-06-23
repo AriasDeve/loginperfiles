a3b4614adff62e001cee15731fd99fc957a04113
AriasDeve <alexander171194@gmail.com>
2023-06-23 09:27:14 -0500

    update2

--- validaruser.php
+++ validaruser.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Validar Usuario</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link rel="stylesheet" href="./prueba.css">
 </head>
+
 <body>
   <div class="container">
     <div class="row">
@@ -57,7 +60,7 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <form class="form-group text-center" action="" autocomplete="off">
+          <form class="form-group text-center" action="" autocomplete="off" id="form-clave">
             <h4>INGRESAR CODIGO</h4>
             <label for="clave" class="form-label text-justify">
               Te enviamos un código a tu e-mail para que puedas restablecer tu contraseña.
@@ -69,17 +72,9 @@
               <input class="inputc" type="tel" id="key3" maxlength="1" onkeyup="onKeyUp(this, 'key4')" />
               <input class="inputc" type="tel" id="key4" maxlength="1" onkeyup="onKeyUp(this, 'comprobar')" />
             </div>
-            <div class="form-group mt-3 d-none" id="keys-false">
-              <input class="inputc"  maxlength="1" value="*" readonly/>
-              <input class="inputc"  maxlength="1" value="*" readonly/>
-              <input class="inputc"  maxlength="1" value="*" readonly/>
-              <input class="inputc"  maxlength="1" value="*" readonly/>
-            </div>
-            <!-- <button onclick="validarClave()">Verificar código</button> -->
-            <!-- <input type="number" maxlength="4" class="form-control" id="clave"> -->
             <div id="inputs-clave" class="d-none">
               <div class="md-3">
-                <label for="clave1" class="form-label">Escribe tu  nueva contraseña:</label>
+                <label for="clave1" class="form-label">Escribe tu nueva contraseña:</label>
                 <input type="password" class="form-control" id="clave1">
               </div>
 
@@ -91,9 +86,9 @@
           </form>
         </div>
         <div class="modal-footer justify-content-lg-center">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
           <button type="button" class="btn btn-success" id="comprobar">Comprobar</button>
-          <button type="button" class="btn btn-sm btn-primary d-none" id="actualizar">Actualizar clave</button>
+          <button type="button" class="btn btn-primary d-none" id="actualizar">Actualizar clave</button>
         </div>
       </div>
     </div>
@@ -101,161 +96,168 @@
 
   <!-- Fin zona de modales -->
   <script src="./prueba.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script> 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+    integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+    crossorigin="anonymous"></script>
 
-<script>
-  document.addEventListener("DOMContentLoaded",() =>{
+  <script>
+    document.addEventListener("DOMContentLoaded", () => {
 
-    //Variable para almacenar IDUSUARIO
-    let idusuario = -1;
+      //Variable para almacenar IDUSUARIO
+      let idusuario = -1;
 
-    //Objeto para manipular al MODAL
-     const modal = new bootstrap.Modal(document.querySelector("#modal-validacion"));
+      //Objeto para manipular al MODAL
+      const modal = new bootstrap.Modal(document.querySelector("#modal-validacion"));
 
-    function buscador(){
-      let parametros = new URLSearchParams();
-      parametros.set("operacion","searchUser")
-      parametros.set("user",document.querySelector("#user").value)
+      function buscador() {
+        let parametros = new URLSearchParams();
+        parametros.set("operacion", "searchUser")
+        parametros.set("user", document.querySelector("#user").value)
 
-      fetch(`./controllers/Usuario.controller.php`,{
-        method:'POST',
-        body:parametros
+        fetch(`./controllers/Usuario.controller.php`, {
+          method: 'POST',
+          body: parametros
 
-      })
-        .then(respuesta => respuesta.text())
-        .then(datos => {
-          if(datos != ""){
-            const registro = JSON.parse(datos)
-            //Enviando datos a formulario
-            idusuario = registro.idusuario;
-            document.querySelector("#datosuser").value = `${registro.apellidos} ${registro.nombres}`;
-            document.querySelector("#email").value = registro.email;
-          }else{
-            alert("Usuario no encontrado");
-            idusuario = -1;
-            document.querySelector("#datosuser").value = '';
-            document.querySelector("#email").value = '';
+        })
+          .then(respuesta => respuesta.text())
+          .then(datos => {
+            if (datos != "") {
+              const registro = JSON.parse(datos)
+              //Enviando datos a formulario
+              idusuario = registro.idusuario;
+              document.querySelector("#datosuser").value = `${registro.apellidos} ${registro.nombres}`;
+              document.querySelector("#email").value = registro.email;
+            } else {
+              alert("Usuario no encontrado");
+              idusuario = -1;
+              document.querySelector("#datosuser").value = '';
+              document.querySelector("#email").value = '';
+            }
+          });
+      }
+
+      function generarEnviarCodigo() {
+        const parametros = new URLSearchParams()
+        parametros.append("operacion", "enviarCorreo");
+        parametros.append("email", document.querySelector("#email").value);
+        parametros.append("idusuario", idusuario);
+
+        fetch('./controllers/Usuario.controller.php', {
+          method: 'POST',
+          body: parametros
+        })
+          .then(respuesta => respuesta.json())
+          .then(datos => {
+            console.log(datos);
+            alert(datos.mensaje);
+          });
+      }
+
+      function validarClave() {
+        const parametros = new URLSearchParams();
+        const key1 = document.getElementById("key1").value;
+        const key2 = document.getElementById("key2").value;
+        const key3 = document.getElementById("key3").value;
+        const key4 = document.getElementById("key4").value;
+        const enterCode = `${key1}${key2}${key3}${key4}`;
+        parametros.append("operacion", "validarClave");
+        parametros.append("idusuario", idusuario);
+        parametros.append("clavegenerada", enterCode);
+
+        fetch(`./controllers/Usuario.controller.php`, {
+          method: 'POST',
+          body: parametros
+
+        })
+          .then(respuesta => respuesta.json())
+          .then(datos => {
+            console.log(datos);
+            //Analizando los datos
+            if (datos.status == "PERMITIDO") {
+              document.querySelector("#comprobar").classList.add("d-none");
+              document.querySelector("#inputs-clave").classList.remove("d-none");
+              document.querySelector("#actualizar").classList.remove("d-none");
+              document.querySelector("#comprobar").classList.add("d-none");
+            } else {
+              alert("Clave incorrecta, revise su correo por favor");
+            }
+          });
+      }
+
+      function actualizarClave() {
+        const clave1 = document.querySelector("#clave1").value;
+        const clave2 = document.querySelector("#clave2").value;
+
+        //Si ninguna caja esta vacia
+        if (clave1 != "" && clave2 != "") {
+          if (clave1 == clave2) {
+            const parametros = new URLSearchParams();
+            parametros.append("operacion", "actualizarClave");
+            parametros.append("idusuario", idusuario);
+            parametros.append("claveacceso", clave1);
+            fetch(`./controllers/Usuario.controller.php`, {
+              method: 'POST',
+              body: parametros
+            })
+              .then(respuesta => respuesta.json())
+              .then(datos => {
+                alert("Se actualizó su clave.. vuelva a iniicar sesión");
+                window.location.href = './index.php';
+              });
+
           }
-        });
-    }
-
-    function generarEnviarCodigo(){
-      const parametros = new URLSearchParams()
-      parametros.append("operacion","enviarCorreo");
-      parametros.append("email",document.querySelector("#email").value);
-      parametros.append("idusuario",idusuario);
-
-      fetch('./controllers/Usuario.controller.php',{
-        method: 'POST',
-        body: parametros
-      })
-        .then(respuesta => respuesta.json())
-        .then(datos => {
-          console.log(datos);
-          alert(datos.mensaje);
-        });
-    }
-
-    function validarClave(){
-      const parametros = new URLSearchParams();
-      const key1 = document.getElementById("key1").value;
-      const key2 = document.getElementById("key2").value;
-      const key3 = document.getElementById("key3").value;
-      const key4 = document.getElementById("key4").value;
-      const enterCode = `${key1}${key2}${key3}${key4}`;
-      parametros.append("operacion","validarClave");
-      parametros.append("idusuario",idusuario);
-      parametros.append("clavegenerada", enterCode);
-
-      fetch(`./controllers/Usuario.controller.php`,{
-        method: 'POST',
-        body: parametros
-
-      })
-        .then(respuesta => respuesta.json())
-        .then(datos => {
-          console.log(datos);
-          //Analizando los datos
-          if(datos.status == "PERMITIDO"){
-            document.querySelector("#comprobar").classList.add("d-none");
-            document.querySelector("#keys").classList.add("d-none");
-            document.querySelector("#keys-false").classList.remove("d-none");
-            document.querySelector("#inputs-clave").classList.remove("d-none");
-            document.querySelector("#actualizar").classList.remove("d-none");
-            document.querySelector("#keys").classList.add("d-none");
-            document.querySelector("#keys-false").classList.remove("d-none");
-            document.querySelector("#comprobar").classList.add("d-none");
-          }else{
-            alert("Clave incorrecta, revise su correo por favor");
-          }
-        });
-    }
-
-    function actualizarClave(){
-      const clave1 = document.querySelector("#clave1").value;
-      const clave2 = document.querySelector("#clave2").value;
-
-      //Si ninguna caja esta vacia
-      if(clave1 != "" && clave2 != ""){
-        if(clave1 == clave2){
-          const parametros = new URLSearchParams();
-          parametros.append("operacion","actualizarClave");
-          parametros.append("idusuario",idusuario);
-          parametros.append("claveacceso", clave1);
-          fetch(`./controllers/Usuario.controller.php`,{
-            method: 'POST',
-            body : parametros
-          })
-            .then(respuesta => respuesta.json())
-            .then(datos => {
-              alert("Se actualizó su clave.. vuelva a iniicar sesión");
-              window.location.href = './index.php';
-            });
-         
         }
+
       }
 
-    }
-    //Evento nativo del MODAL....  "Al abrir el modal"
-    modal._element.addEventListener("shown.bs.modal",()=>{
-      document.querySelector("#key1").focus();
-    });
+      //Evento nativo del MODAL....  "Al abrir el modal"
+      modal._element.addEventListener("shown.bs.modal", () => {
+        document.querySelector("#key1").focus();
+      });
 
-     //Evento nativo del MODAL....  "Al cerrar el modal"
-    modal._element.addEventListener("hidden.bs.modal",()=>{
-      document.querySelector("#form-clave").reset();
-    });
+      //Evento nativo del MODAL....  "Al cerrar el modal"
+      modal._element.addEventListener("hidden.bs.modal", () => {
+        document.querySelector("#form-clave").reset();
+        document.querySelector("#inputs-clave").classList.add("d-none");
+        document.querySelector("#comprobar").classList.remove("d-none");
+        document.querySelector("#actualizar").classList.add("d-none");
+      });
 
-    //Evento click para abrir MODAL
-    document.querySelector("#enviar").addEventListener("click",()=>{
-      if(idusuario != -1){
-        generarEnviarCodigo();
-        modal.toggle();  
-      }else{
-        alert("Debe buscar el nombre de usuario");
-        document.querySelector("#user").focus();
-      }      
-    });
+      //Evento click para abrir MODAL
+      document.querySelector("#enviar").addEventListener("click", () => {
+        if (idusuario != -1) {
+          generarEnviarCodigo();
+          modal.toggle();
+        } else {
+          alert("Debe buscar el nombre de usuario");
+          document.querySelector("#user").focus();
+        }
+      });
 
-    //Evento click para bóton
-    document.querySelector("#buscar").addEventListener("click",buscador);
-    document.querySelector("#comprobar").addEventListener("click", validarClave);
-    document.querySelector("#actualizar").addEventListener("click", actualizarClave);
+      //Evento click para bóton
+      document.querySelector("#buscar").addEventListener("click", buscador);
+      document.querySelector("#comprobar").addEventListener("click", validarClave);
+      document.querySelector("#actualizar").addEventListener("click", actualizarClave);
 
-    //Evento keypress(ENTER) caja de texto
-    document.querySelector("#user").addEventListener("keypress",(key) =>{
-      if(key.keyCode == 13){
-        buscador();
-      }
+      //Evento keypress(ENTER) caja de texto
+      document.querySelector("#user").addEventListener("keypress", (key) => {
+        if (key.keyCode == 13) {
+          buscador();
+        }
+      })
+
     })
-
-  })
-</script>
+  </script>
 
 
 
 
 </body>
+
 </html>
